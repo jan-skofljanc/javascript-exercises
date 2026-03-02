d64f6b99414f88387f15966d5bb807ee7f66f7d3
jan-skofljanc <42697383+jan-skofljanc@users.noreply.github.com>
2026-03-03 00:26:42 +0100

    completed exercise 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divisibleByFour = year % 4 === 0;
+    let divisibleByHundred = year % 100 === 0;
+    let divisibleByFourhundred = year % 400 === 0;
 
+    if (divisibleByFour && (divisibleByFourhundred || !divisibleByHundred)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
