2beff87d464bd257a07615bdf21c8e07bdcfab7b
jan-skofljanc <42697383+jan-skofljanc@users.noreply.github.com>
2026-03-02 23:07:56 +0100

    completed exercise 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    var splitStr = str.split(""); // creates and array of each letter
+    var reverseStr = splitStr.reverse(); // reverses the array
+    var joinStr = reverseStr.join(""); // joins the array back to a string
 
+    return joinStr;
 };
 
 // Do not edit below this line
