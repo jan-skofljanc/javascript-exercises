b21dac653dee9e0279af19fb9882eaf0205e970b
jan-skofljanc <42697383+jan-skofljanc@users.noreply.github.com>
2026-03-03 00:37:29 +0100

    completed exercise 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let convertedC = (tempF - 32) / 1.8;
+  return Math.round(convertedC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let convertedF = tempC * 1.8 + 32;
+  return Math.round(convertedF * 10) / 10;
 };
 
 // Do not edit below this line
