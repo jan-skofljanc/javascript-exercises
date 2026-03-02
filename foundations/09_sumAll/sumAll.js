50eb4a52145a8d9afe45c24cdf1760798f1f220e
jan-skofljanc <42697383+jan-skofljanc@users.noreply.github.com>
2026-03-03 00:00:46 +0100

    completed exercise 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start > end) {
+        let temporaryNum = start;
+        start = end;
+        end = temporaryNum;
+    }
+    if (start < 0) {
+        return "ERROR";
+    }
+    if (end < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(start)) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(end)) {
+        return "ERROR";
+    }
+    
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum +=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
