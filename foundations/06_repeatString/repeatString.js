d51dc7dd099709261c9527af4bb53fad907a7003
jan-skofljanc <42697383+jan-skofljanc@users.noreply.github.com>
2026-03-02 22:59:01 +0100

    completed exercise 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+    return "ERROR";
+    }
+    
+    let newstring = "";
+    
+    for (let i = 0; i < num; i++) {
+        newstring += str;
+    }
+    return newstring;
 };
 
 // Do not edit below this line
